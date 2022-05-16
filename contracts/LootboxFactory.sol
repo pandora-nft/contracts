6b264a6d2d0b9632e92cc756e144a9b03b44081e
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-16 22:14:19 +0700

    feat: getLootboxesOwnedByUser

--- contracts/LootboxFactory.sol
+++ contracts/LootboxFactory.sol
@@ -230,4 +230,19 @@ contract LootboxFactory is Ownable, KeeperCompatible, VRFConsumerBaseV2 {
     function getAllLootboxes() public view returns (address[] memory) {
         return allLootboxes;
     }
+
+    function getLootboxesOwnedByUser(address _user)
+        public
+        view
+        returns (address[] memory)
+    {
+        uint256[] memory lootboxIds = getLootboxOwned(_user);
+
+        address[] memory lootboxes = new address[](lootboxIds.length);
+        for (uint256 i = 0; i < lootboxIds.length; i++) {
+            uint256 lootboxId = lootboxIds[i];
+            lootboxes[i] = allLootboxes[lootboxId];
+        }
+        return lootboxes;
+    }
 }
