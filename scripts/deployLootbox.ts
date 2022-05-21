af5fe15f8a8a93c9fa55bbabfc97e4460d2140f2
Krittamook Aksornchindarat <krittamook@hotmail.com>
2022-05-21 13:22:14 +0700

    feat: deployLootbox script

---
+++ scripts/deployLootbox.ts
@@ -0,0 +1,25 @@
+import { LOOTBOX_FACTORY } from "../constants/address";
+import { ethers, network } from "hardhat";
+
+const deployLootbox = async () => {
+  const drawTime = new Date(Date.now() + 3600 * 24 * 2).getTime();
+
+  const LootboxFactory = await ethers.getContractFactory("LootboxFactory");
+  const lootboxFactory = await LootboxFactory.attach(LOOTBOX_FACTORY[80001]);
+
+  const tx = await lootboxFactory[
+    "deployLootbox(string,uint256,uint256,uint256)"
+  ](
+    "Bird lootbox 2",
+    drawTime,
+    ethers.utils.parseEther("0.01"),
+    ethers.utils.parseEther("0")
+  );
+  await tx.wait();
+
+  const lootboxAddress = (await lootboxFactory.functions.lootboxAddress(0))[0];
+
+  console.log("new lootbox deployed at", lootboxAddress);
+};
+
+deployLootbox();
