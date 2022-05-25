4d96791730c9c6c71d4ee67bee5f4617a4acd0b4
pannavich <pannavich1911@gmail.com>
2022-05-25 18:36:32 +0700

    edit rinkeby address

--- constants/address.ts
+++ constants/address.ts
@@ -1,6 +1,6 @@
 export const LOOTBOX_FACTORY: { [chainId: number]: string } = {
     //Rinkeby
-    4: "0xedF72393De2860E0cEc3C1D48865c3D58957556F",
+    4: "0x53D5A5Ee0DC444DcB681fF11ae8a630e79F2A577",
     //BNB Testnet
     97: "0x86490bA09C96F36f288A618E0f11b09A97E427Da",
     //Mumbai
@@ -14,10 +14,9 @@ export const LOOTBOX_FACTORY: { [chainId: number]: string } = {
     //Polygon Mainnet
     // 137: "",
 }
-
 export const TICKET: { [chainId: number]: string } = {
     //Rinkeby
-    4: "0xe5674c9166CAaA4aFb43102aaDAE115910d62eE1",
+    4: "0xe1B562e5C935f6a9cBB732F97aF3d57ffE3875d0",
     //BNB Testnet
     97: "0x624b980A80008307939aD769D5e0C2a8C2d25DE1",
     //Mumbai
@@ -35,7 +34,7 @@ export const TICKET: { [chainId: number]: string } = {
 
 export const TEST_LOOTBOX: { [chainId: number]: string } = {
     //Rinkeby
-    4: "0x52c608b955Bcf424cD5D37136b6bA99E7E6DafdF",
+    4: "0xEe966B1186B829271F0B9760B293d586a806F1F0",
     //BNB Testnet
     97: "0x910cEebd0555d10F1A84a673e924197f0Fdd30Bb",
     //Mumbai
